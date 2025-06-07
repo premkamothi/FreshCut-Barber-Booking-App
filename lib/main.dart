e753cbd494172542aff1779aab626dd7a32413a7
Prem Kamothi <premkamothi16@gmail.com>
2025-06-07 15:51:48 +0530

    your message

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,17 @@
-import "package:firebase_core/firebase_core.dart";
-import "package:flutter/material.dart";
-import "package:project_sem7/uiscreen/StartingPage.dart";
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:project_sem7/uiscreen/StartingPage.dart';
+import 'firebase_options.dart'; // Import this
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform, // Use this
+  );
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
