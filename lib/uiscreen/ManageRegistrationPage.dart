233da839d245e1d8dbdf83cc1626547beefcc430
dhruvmalli <dp717270@gmail.com>
2025-07-11 18:45:31 +0530

    Sign for customer and owner complete

---
+++ lib/uiscreen/ManageRegistrationPage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Manageregistrationpage extends StatefulWidget {
+  const Manageregistrationpage({super.key});
+
+  @override
+  State<Manageregistrationpage> createState() => _ManageregistrationpageState();
+}
+
+class _ManageregistrationpageState extends State<Manageregistrationpage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
